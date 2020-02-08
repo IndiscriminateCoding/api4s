732bf8bde197465f48767d7c141d2d008c92db1c
Scala Steward <me@scala-steward.org>
2020-02-09 00:26:47 +0100

    Update http4s-circe, http4s-core to 0.21.0-RC5

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object Versions {
   val api4s = "0.2.5-SNAPSHOT"
-  val http4s = "0.21.0-RC4"
+  val http4s = "0.21.0-RC5"
   val circe = "0.13.0-RC1"
 }
--- tests/project/Versions.scala
+++ tests/project/Versions.scala
@@ -1,4 +1,4 @@
 object Versions {
   val api4s = "0.2.5-SNAPSHOT"
-  val http4s = "0.21.0-RC4"
+  val http4s = "0.21.0-RC5"
 }
